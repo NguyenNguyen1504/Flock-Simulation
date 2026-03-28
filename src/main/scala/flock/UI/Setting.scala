f045b9a4ad2e8da63d24ebdc63a3bf92123f25ce
Nguyen Nguyen <nvtnguyen2007@gmail.com>
2026-03-28 20:52:14 +0200

    Created classes for slider and spinner setting fields.

--- src/main/scala/flock/UI/Setting.scala
+++ src/main/scala/flock/UI/Setting.scala
@@ -1,11 +1,12 @@
 package flock.UI
 
 import scalafx.geometry.Pos
-import scalafx.scene.control.Label
+import scalafx.scene.control.{Label, Slider, Spinner}
 import scalafx.scene.layout.HBox
+import scalafx.Includes._
 
 trait Setting extends HBox:
-  
+
   spacing = 10
   alignment = Pos.CenterLeft
   val labelName: String
@@ -13,3 +14,25 @@ trait Setting extends HBox:
 
 end Setting
 
+class SliderSetting(val labelName: String, minValue: Double, maxValue: Double, initial: Double) extends Setting:
+
+  val slider = new Slider(minValue, maxValue, initial)
+  children.add(slider)
+  def onChange(action: Double => Unit) =
+    slider.value.onChange((_, _, v) => action(v.doubleValue()))
+
+end SliderSetting
+
+class SpinnerSetting(val labelName: String, minValue: Int, maxValue: Int, initial: Int) extends Setting:
+
+  val spinner = new Spinner[Int](minValue, maxValue, initial):
+    editable = true // Cho phép gõ số bằng tay
+
+  // Trick để cập nhật giá trị ngay khi người dùng nhấn Enter hoặc mất focus
+  spinner.focusedProperty().onChange((_, _, isFocused) => {
+    if (!isFocused) spinner.increment(0)
+  })
+  children.add(spinner)
+  def onChange(action: Int => Unit) = spinner.valueProperty().onChange((_, _, v) => action(v))
+
+end SpinnerSetting
