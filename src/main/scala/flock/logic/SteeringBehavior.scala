43587efb9082aa3b36ffee5a791b5c1e9f2485e6
Nguyen Nguyen <nvtnguyen2007@gmail.com>
2026-04-13 21:21:41 +0300

    Create a trait SteeringBehavior and implement 3 steering rules calculation.

---
+++ src/main/scala/flock/logic/SteeringBehavior.scala
@@ -0,0 +1,40 @@
+package flock.logic
+
+trait SteeringBehavior:
+  def calculate(target: Boid, neighbors: Seq[Boid], constants: Constants): Vector2D
+end SteeringBehavior
+
+
+object Separation extends SteeringBehavior:
+  def calculate(target: Boid, allBoids: Seq[Boid], constants: Constants): Vector2D =
+    // Take all surrounding boids, not only the ones in the perception angle
+    val minDSquared = constants.minSeparationDistance * constants.minSeparationDistance
+    val closeNeighbors = allBoids.filter(b => b != target && b.distanceSquared(target) <= minDSquared)
+
+    // Avoid UnsupportedOperationException
+    if closeNeighbors.isEmpty then Vector2D(0,0)
+    else
+      closeNeighbors.map(another => {
+        // Calculates a repulsion vector inversely proportional to the squared distance
+        val toAnother = target.position - another.position
+        toAnother * (1.0 / toAnother.magnitudeSquared())
+      }).reduce(_+_)
+end Separation
+
+
+object Alignment extends SteeringBehavior:
+  def calculate(target: Boid, neighbors: Seq[Boid], constants: Constants): Vector2D =
+    if neighbors.isEmpty then Vector2D(0,0)
+    else
+      val avgVelocity = neighbors.map(_.velocity).reduce(_+_) * (1.0 / neighbors.size)
+      avgVelocity - target.velocity
+end Alignment
+
+
+object Cohesion extends SteeringBehavior:
+  def calculate(target: Boid, neighbors: Seq[Boid], constants: Constants): Vector2D =
+    if neighbors.isEmpty then Vector2D(0,0)
+    else
+      val centerOfMass = neighbors.map(_.position).reduce(_+_) * (1.0 / neighbors.size)
+      centerOfMass - target.position
+end Cohesion
